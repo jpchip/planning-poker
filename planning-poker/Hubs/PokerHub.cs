7b7937eff72392dbec34217d8891a4f3b4d175af
jpchapiewsky <jpchapiewsky@gmail.com>
2021-02-21 16:07:58 -0600

    set up basic snowpack setup

---
+++ planning-poker/Hubs/PokerHub.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace planning_poker.Hubs
+{
+    public class PokerHub : Hub
+    {
+    }
+}
--- planning-poker/Startup.cs
+++ planning-poker/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using planning_poker.Hubs;
 
 namespace planning_poker
 {
@@ -16,6 +17,7 @@ namespace planning_poker
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,13 +29,16 @@ namespace planning_poker
             }
 
             app.UseRouting();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
+                endpoints.MapHub<PokerHub>("/hub");
+                /*endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                    await context.Response.WriteAsync("Hello World!!");
+                });*/
             });
         }
     }
