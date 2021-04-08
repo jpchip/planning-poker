118881c10c92e506f9e47997759445d7a2278f77
jpchapiewsky <jpchapiewsky@gmail.com>
2021-04-07 21:30:55 -0500

    add emoji for cards

--- src/js/components/ScoreDisplay.svelte
+++ src/js/components/ScoreDisplay.svelte
@@ -7,6 +7,36 @@
       th, td {
         text-align: center;
       }
+
+      span[role=img][aria-label],
+      span[role=img][aria-label] {
+        position: relative;
+      }
+
+      span[role=img][aria-label]:focus::after,
+      span[role=img][aria-label]:hover::after {
+        position: absolute;
+        display: block;
+        z-index: 1;
+        bottom: 1.5em;
+        left: 0;
+        max-width: 5em;
+        padding: .5em .75em;
+        border: .05em solid rgba(255,255,255,1);
+        border-radius: .2em;
+        box-shadow: .15em .15em .5em rgba(0,0,0,1);
+        content: attr(aria-label);
+        background-color: rgba(0,0,0,.85);
+        color: rgba(255,255,255,1);
+        font-size: 80%;
+        animation: TOOLTIP .1s ease-out 1;
+      }
+
+      @media print {
+        span[role=img][aria-label]::after {
+          content: " (" attr(aria-label) ") ";
+        }
+      }
     </style>
       <table class="table table-bordered my-2">     
         <thead>
@@ -25,7 +55,7 @@
                   {user.score}
                 {:else}
                   {#if user.score > 0}
-                    ?
+                  <span role="img" aria-label="Score selected">🎴</span>
                   {:else}
                     -
                   {/if}
